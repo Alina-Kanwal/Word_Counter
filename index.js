581c1bc73338c25a80d0f7db41fb611695cd03b1
Alina-Kanwal <164280493+Alina-Kanwal@users.noreply.github.com>
2024-05-03 03:41:47 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+//This line is called shabang it tell operating system to run with node.js
+// here we import inquirer
+import inquirer from "inquirer";
+//declare a constant "answers" and assign it to the result ofinquirer.prompt function
+const answers = await inquirer.prompt([
+    { name: "sentence",
+        type: "input",
+        message: "Enter your sentence to create the word!"
+    }
+]);
+const words = answers.sentence.trim().split(' ');
+//print the array of words to console
+console.log(words);
+//print the word count of the sentence to the console
+console.log(`You wrote total ${words.length} words`);
